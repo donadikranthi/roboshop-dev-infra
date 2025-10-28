95c1c7e5afaec178924951bd55189c7801beeb8a
donadikranthi <donadikranthikumar@gmail.com>
2025-10-28 18:16:15 +0530

    main

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,13 +1,13 @@
 # Using Open source module
-/* module "catalogue" {
-  source = "terraform-aws-modules/security-group/aws"
+# module "catalogue" {
+#   source = "terraform-aws-modules/security-group/aws"
 
-  name        = "${local.common_name_suffix}-catalogue"
-  use_name_prefix = false
-  description = "Security group for catalogue with custom ports open within VPC, egress all traffic"
-  vpc_id      = data.aws_ssm_parameter.vpc_id.value
+#   name        = "${local.common_name_suffix}-catalogue"
+#   use_name_prefix = false
+#   description = "Security group for catalogue with custom ports open within VPC, egress all traffic"
+#   vpc_id      = data.aws_ssm_parameter.vpc_id.value
 
-} */
+# } 
 
 
 module "sg" {
@@ -19,13 +19,3 @@ module "sg" {
   sg_description = "Created for ${var.sg_names[count.index]}"
   vpc_id =  local.vpc_id
 }
-
-# Frontend accepting traffic from frontend ALB
-# resource "aws_security_group_rule" "frontend_frontend_alb" {
-#   type              = "ingress"
-#   security_group_id = module.sg[9].sg_id # frontend SG ID
-#   source_security_group_id = module.sg[11].sg_id # frontend ALB SG ID
-#   from_port         = 80
-#   protocol          = "tcp"
-#   to_port           = 80
-# }
--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -7,18 +7,7 @@ variable "environment" {
 }
 
 variable "sg_names" {
-    default = [
-        # databases
-        "mongodb", "redis", "mysql", "rabbitmq",
-        # backend
-        "catalogue", "user", "cart", "shipping", "payment",
-        # frontend
-        "frontend",
-        # bastion
-        "bastion",
-        # frontend load balancer
-        "frontend_alb",
-        # Backend ALB
-        "backend_alb"
-    ]
+    default = [ 
+        "mongodb", "redis", "mysql", "rabbitmq" ]
 }
+       
