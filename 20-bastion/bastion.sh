aa57d728fa088c92786d2aebc974b8e2784a5a93
Kranthi <109164488+donadikranthi@users.noreply.github.com>
2025-11-16 07:42:27 +0530

    Update bastion.sh

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -9,7 +9,7 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
 
-# sudo lvreduce -r -L 6G /dev/mapper/RootVG-rootVol
+sudo lvreduce -r -L 6G /dev/mapper/RootVG-rootVol
 
 # creating databases
 cd /home/ec2-user
