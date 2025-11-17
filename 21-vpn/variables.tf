7d87b57389ffab2aa2e47e6683e24b80e55b2d25
Kranthi <109164488+donadikranthi@users.noreply.github.com>
2025-11-17 11:54:38 +0530

    Create variables.tf

---
+++ 21-vpn/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z04572451B9X32T33PSC8"
+}
+
+variable "domain_name" {
+    default = "kranthi.fun"
+}
