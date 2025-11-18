0404853026c2cd924c53fbec3d154c9d1422ea43
donadikranthi <donadikranthikumar@gmail.com>
2025-11-18 20:23:56 +0530

    main

---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "kranthi.fun"
+}
+
+variable "zone_id" {
+    default = "Z04572451B9X32T33PSC8"
+}
